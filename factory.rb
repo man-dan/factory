e1b242a2e4e3dc9fc0ee780cede215e73d176f8f
Den <man-dan@mail.ru>
2016-05-18 10:46:21 +0300

    Main methods

--- factory.rb
+++ factory.rb
@@ -12,6 +12,21 @@ class Factory
         args
       end
 
+      def values
+        instance_variables.map { |arg| instance_variable_get("#{arg}") }
+      end
+
+      def ==(another)
+        self.class == another.class && self.values == another.values 
+      end
+
+      def [](arg)
+        arg.is_a?(Fixnum) ? "#{args[arg]}" : instance_variable_get("@#{arg}")
+      end
+
+      def []=(arg, val)
+        arg.is_a?(Fixnum) ? "#{args[arg]=val}" : instance_variable_set("@#{arg}", val)
+      end
     end
   end
 end
--- test.rb
+++ test.rb
@@ -3,4 +3,7 @@ require './factory.rb'
 test = Factory.new(:name, :address, :zip)
 pod_test = test.new("Den","Universal street", 221)
 pod_test2 = test.new("Ivan","Karuna street", 263)
-puts pod_test2.address
+pod_test3 = test.new("Den","Universal street", 221)
+puts pod_test3==pod_test
+puts pod_test3[1]
+puts pod_test3[-1]= 100
